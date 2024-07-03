4ba895a393d8b9766895d3180182872890d11307
root <root@localhost.localnet>
2024-07-03 11:53:10 +0300

    * adds vpc with public subnet

--- modules/base/main.tf
+++ modules/base/main.tf
@@ -35,3 +35,21 @@ resource "aws_internet_gateway" "ig" {
     Name = "Mongo Internet Gateway"
   }
 }
+
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.vpc_main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+
+  tags = {
+    Name = "Public route table"
+  }
+}
+
+resource "aws_route_table_association" "public_asso" {
+  count          = length(var.cidr_subnet_public)
+  subnet_id      = element(aws_subnet.subnet_public[*].id, count.index)
+  route_table_id = aws_route_table.rt_public.id
+}
