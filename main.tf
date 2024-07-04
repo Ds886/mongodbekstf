c9efc608eac2c5e474703742716010bb4e53434a
root <root@localhost.localnet>
2024-07-04 20:00:41 +0300

    * current state eks

--- main.tf
+++ main.tf
@@ -1,30 +1,13 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-  owners = ["099720109477"] #canonical
-}
-
 module "base_vpc" {
   source = "./modules/base"
 }
 
-# https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v20.16.0/examples/eks-managed-node-group/eks-al2.tf
-module "eks_al2" {
+# source: https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/examples/eks-managed-node-group/eks-al2023.tf
+module "eks_al2023" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "mongo-al2"
+  cluster_name    = "da-mongoeks-al2023"
   cluster_version = "1.30"
 
   # EKS Addons
@@ -41,16 +24,13 @@ module "eks_al2" {
   eks_managed_node_groups = {
     example = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2_x86_64"
-      instance_types = ["m6i.large"]
+      instance_types = ["m5i.large"]
 
-      min_size     = 2
-      max_size     = 5
+      min_size = 2
+      max_size = 5
+      # This value is ignored after the initial creation
+      # https://github.com/bryantbiggs/eks-desired-size-hack
       desired_size = 2
     }
-  }
-
-  tags = {
-    Name = "Mongo_EKS"
   }
 }
