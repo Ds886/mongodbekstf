57d72b2726e5b2b7e9245b58a91f1cd28bbac64b
root <root@localhost.localnet>
2024-07-04 18:08:04 +0300

    * initial eks

--- main.tf
+++ main.tf
@@ -18,3 +18,39 @@ data "aws_ami" "ubuntu" {
 module "base_vpc" {
   source = "./modules/base"
 }
+
+# https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v20.16.0/examples/eks-managed-node-group/eks-al2.tf
+module "eks_al2" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "mongo-al2"
+  cluster_version = "1.30"
+
+  # EKS Addons
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id     = module.base_vpc.vpc_id
+  subnet_ids = concat(module.base_vpc.subnet_private_id, module.base_vpc.subnet_public_id)
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2_x86_64"
+      instance_types = ["m6i.large"]
+
+      min_size     = 2
+      max_size     = 5
+      desired_size = 2
+    }
+  }
+
+  tags = {
+    Name = "Mongo_EKS"
+  }
+}
